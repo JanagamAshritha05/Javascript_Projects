c1181276b1de57656ab3873373661fd377ab765e
Ashritha <janagamashrithareddy@gmail.com>
2026-03-31 22:29:05 +0530

    updated todoList

--- TodoList/todoList.js
+++ TodoList/todoList.js
@@ -40,4 +40,34 @@ function showTask(){
 showTask();
 
 
+/*
+Difference between innerHTML and textContent
+
+innerHTML:
+- Gets or sets HTML content inside an element
+- innerHTML understands HTML tags
+- Slower and less secure (can cause XSS issues)
+
+Example:
+h1.innerHTML = "<b>Hello</b>";
+Output: Hello (bold)
+
+textContent:
+- Gets or sets only text content
+- HTML tags are not understand using textContent
+- Faster and safer (recommended)
+
+Example:
+h1.textContent = "<b>Hello</b>";
+Output: <b>Hello</b> (normal text)
+
+When to use:
+- Use textContent → when changing only text (recommended)
+- Use innerHTML → when inserting HTML elements
+
+
+*/
+
+
+
 
